d7f002f5e9aec28229edc61062c7d1c7cfca867b
C-D-Lewis <Bonsitm@gmail.com>
2015-08-21 13:16:57 +0100

    Initial code commit

---
+++ arduino/smartstrap/smartstrap.ino
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+#include <ArduinoPebbleSerial.h>
+
+#define BUTTON_PIN        7
+#define DATA_PIN          8
+#define DEBOUNCE_INTERVAL 500
+
+static uint8_t s_data_buffer[128];
+static long s_last_time = 0;
+
+static const uint16_t s_service_ids[] = {0x0};
+static const uint16_t s_attr_ids[] = {0x0};
+
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  
+  // Setup the Pebble smartstrap connection using one wire software serial
+  ArduinoPebbleSerial::begin_software(DATA_PIN, s_data_buffer, sizeof(s_data_buffer),
+                                      Baud9600, s_service_ids, 1);
+}
+
+void loop() {
+  size_t length;
+  bool is_read;
+  uint16_t service_id;
+  uint16_t attribute_id;
+  
+  // Check for frames, though none are expected for this app
+  ArduinoPebbleSerial::feed(&service_id, &attribute_id, &length, &is_read);
+
+  // If the button is pressed...
+  if(digitalRead(BUTTON_PIN) == HIGH) {
+    long now = millis();
+    if(now - s_last_time > DEBOUNCE_INTERVAL) {
+      s_last_time = now;
+     
+      // Notify the watch!
+      ArduinoPebbleSerial::notify(s_service_ids[0], s_attr_ids[0]);
+    }
+  }
+
+  // Show connected status on the LED
+  digitalWrite(13, ArduinoPebbleSerial::is_connected() ? HIGH : LOW);
+}
---
+++ pebble/src/main.c
@@ -0,0 +1,89 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_output_layer;
+
+SmartstrapAttribute *attribute;
+
+static int s_button_presses;
+static bool s_service_available;
+
+static void update_ui();
+
+/******************************** Smartstraps *********************************/
+
+static void strap_availability_handler(SmartstrapServiceId service_id, bool is_available) {
+  // A service's availability has changed
+  APP_LOG(APP_LOG_LEVEL_INFO, "Service %d is %s available", (int)service_id, is_available ? "now" : "NOT");
+
+  // Remember if the raw service is available
+  s_service_available = (is_available && service_id == SMARTSTRAP_RAW_DATA_SERVICE_ID);
+  update_ui();
+}
+
+static void strap_notify_handler(SmartstrapAttribute *attribute) {
+  // Button was pressed!
+  s_button_presses++;
+  update_ui();
+  vibes_short_pulse();
+}
+
+static void strap_init() {
+  attribute = smartstrap_attribute_create(SMARTSTRAP_RAW_DATA_SERVICE_ID, SMARTSTRAP_RAW_DATA_ATTRIBUTE_ID, 64);
+
+  // Subscribe to smartstrap events
+  smartstrap_subscribe((SmartstrapHandlers) {
+    .availability_did_change = strap_availability_handler,
+    .notified = strap_notify_handler
+  });
+}
+
+/************************************* UI *************************************/
+
+static void update_ui() {
+  static char s_buffer[128];
+  snprintf(s_buffer, sizeof(s_buffer), "Connected: %s\n\nButton presses:\n%d",
+    (s_service_available ? "YES" : "NO"), s_button_presses);
+  text_layer_set_text(s_output_layer, s_buffer);
+}
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_output_layer = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+  text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
+  text_layer_set_text_color(s_output_layer, GColorWhite);
+  text_layer_set_background_color(s_output_layer, GColorClear);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_output_layer));
+}
+
+static void main_window_unload(Window *window) {
+  text_layer_destroy(s_output_layer);
+  window_destroy(s_main_window);
+}
+
+/************************************ App *************************************/
+
+static void init() {
+  strap_init();
+
+  s_main_window = window_create();
+  window_set_background_color(s_main_window, GColorBlack);
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
+
+  update_ui();
+}
+
+static void deinit() { }
+
+int main() {
+  init();
+  app_event_loop();
+  deinit();
+}
